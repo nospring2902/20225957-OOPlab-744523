50d1660520e33adb51ce81249b2234308e43e694
nospring2902 <163527486+nospring2902@users.noreply.github.com>
2024-09-29 21:54:31 +0700

    Update FirstDialog.java

--- LAB01/FirstDialog.java
+++ LAB01/FirstDialog.java
@@ -1,7 +1,7 @@
 import javax.swing.JOptionPane;
 public class FirstDialog {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null,"Hello, world");
+        JOptionPane.showMessageDialog(null,"Hello world! How are you?");
         System.exit(0);
     }
 }
